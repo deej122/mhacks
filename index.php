74048bb43efe03776c71bde9464f1689f76c8c24
wburglett <asb2211@columbia.edu>
2013-09-21 15:37:14 -0400

    retest step

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 	$db = $m->hack;
 	$collection = $db->users;
 	$userQ = array('username' => $_POST["user"]);
-	$cursor = iterator_to_array($collection->find($userQ));
+	$cursor = iterator_to_array($collection->find($userQ), array('_id' => 0));
 	var_dump(reset($cursor));
 	if (/*(reset($cursor))['password'] == $_POST["password"]*/1) {
 		$collection = $db->tokens;
