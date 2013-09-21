a9ff8cf47671cb6d040205e9bcf77392bc824e68
wburglett <asb2211@columbia.edu>
2013-09-21 15:07:00 -0400

    missing paren

--- index.php
+++ index.php
@@ -27,16 +27,16 @@
   error_reporting(-1); ini_set('display_errors', '1');
   
   if (!isset($_POST["name"])) {
-  echo '<form class="loginForm" action="index.php" method="POST" >
-    <p class="formLabel">Sign Up to find teammates</p>  
-    <input name="name" type="text" class="input" placeholder="Name"/>
-    <br><br>    
-    <input name="cont" type="text" class="input" placeholder="Email/Cell Phone"/>
-    <br><br>
-    <input name="pass" type="password" class="input" placeholder="Password"/>
-    <br><br>
-    <input type="submit" class="button" value="Sign Up"/>
-  </form>';   
+	echo '<form class="loginForm" action="index.php" method="POST" >
+			<p class="formLabel">Sign Up to find teammates</p>  
+			<input name="name" type="text" class="input" placeholder="Name"/>
+			<br><br>    
+			<input name="cont" type="text" class="input" placeholder="Email/Cell Phone"/>
+			<br><br>
+			<input name="pass" type="password" class="input" placeholder="Password"/>
+			<br><br>
+			<input type="submit" class="button" value="Sign Up"/>
+		  </form>';   
   }
   else {
 	$m = new MongoClient();
@@ -46,15 +46,15 @@
 	echo 'Thanks! Now log in';
   }
   
-  if (!isset($_POST["user"]) && $_POST["password"])) {
-  echo '<form class="loginForm" action="index.php" method="POST" id="login">
-    <p class="formLabel">Login to find teammates</p>  
-    <input name="user" type="text" class="input" placeholder="Email/Cell Phone"/>
-    <br><br>
-    <input name="password" type="password" class="input" placeholder="Password"/>
-    <br><br>
-    <input type="submit" class="button" value="Login"/>
-  </form>';
+  if (!isset($_POST["user"]) && !isset(POST["password"])) {
+	echo '<form class="loginForm" action="index.php" method="POST" id="login">
+			<p class="formLabel">Login to find teammates</p>  
+			<input name="user" type="text" class="input" placeholder="Email/Cell Phone"/>
+			<br><br>
+			<input name="password" type="password" class="input" placeholder="Password"/>
+			<br><br>
+			<input type="submit" class="button" value="Login"/>
+		</form>';
   }
   else {
 	$m = new MongoClient();
