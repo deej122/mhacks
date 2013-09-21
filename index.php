4592d0d98f75a92d1bf0c765dcba0dc0d35b6ccf
wburglett <asb2211@columbia.edu>
2013-09-21 15:59:57 -0400

    edit back...

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 	$db = $m->hack;
 	$collection = $db->users;
 	$userQ = array('username' => $_POST["user"]);
-	$cursor = $collection->find($userQ, array('_id' => 0, 'username' => true));
+	$cursor = $collection->find($userQ, array('username' => true));
 	var_dump($cursor);
 	if (/*(reset($cursor))['password'] == $_POST["password"]*/1) {
 		$collection = $db->tokens;
