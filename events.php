87d4b48d1a199fbef6e9fdb7a8a5d2b1e430858d
wburglett <asb2211@columbia.edu>
2013-09-22 00:05:31 -0400

    0 Modal

--- events.php
+++ events.php
@@ -60,7 +60,7 @@
 
 
   <!-- Modal -->
-  <div class="modal fade" id="myModal" tabindex="0" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
